0dc700b4332632019f15dfe84368c3e2c6b0a4a4
Musrat001 <mohdmusrat73@gmail.com>
2026-07-01 03:36:56 +0530

    arranged the file structure in some better way

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -5,22 +5,14 @@ exports.loginLogic = async (req, res) => {
     console.log(req.body);
     const username = req.body.username;
     const password = req.body.password;
-    console.log(process.env.ACCESS_TOKEN_SECRET);
+    
 
     // finding user
     const user = await User.findOne({
         username,
         password
     });
-    console.log(user);
-
-
-    if (!user) {
-        return res.status(402).send({
-            message: "invalid user"
-        })
-    }
-
+    
 
     // generating access Token
     const accessToken = jwt.sign(
@@ -36,12 +28,12 @@ exports.loginLogic = async (req, res) => {
 
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: false,       // true in production with HTTPS
+        secure: false,       
         sameSite: "Lax",
         maxAge: 24 * 60 * 60 * 1000
     });
 
-    return res.json({
+    return res.status(201).json({
         success: true,
         message: "Login successful",
         accessToken: accessToken,
