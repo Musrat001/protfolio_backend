979393010c791ad7199e2dd93fe816d991fc77ed
Musrat001 <mohdmusrat73@gmail.com>
2026-07-01 16:11:13 +0530

    arranged the file structure in some better way

--- src/middlewares/authJWT.middlewares.js
+++ src/middlewares/authJWT.middlewares.js
@@ -1,27 +1,37 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const verifyJWT = async (req, res, next)=>{
-    const currentToken = req.cookies.accessToken;;
-    
+const verifyJWT = (req, res, next) => {
+
+    console.log("Cookies:", req.cookies);
+
+    console.log("Access Token:", req.cookies.accessToken);
+
+    const currentToken = req.cookies.accessToken;
+
     if(!currentToken){
         return res.status(401).json({
-            message: "please provide access token"
-        })
+            message:"please provide access token"
+        });
     }
 
-    jwt.verify(currentToken,  process.env.ACCESS_TOKEN_SECRET,(error, decoded)=>{
+    jwt.verify(currentToken, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+
         if(error){
+            console.log(error);
+
             return res.status(401).json({
-                message: "Unauthorized access, token expired"
-            })
+                message:"Unauthorized access"
+            });
         }
 
-        return email = decoded.email;
-    })
+        req.user = decoded;
 
-    next();
+        next();
+    });
+
 }
 
-module.exports ={
+module.exports = {
     verifyJWT: verifyJWT
 }
+
